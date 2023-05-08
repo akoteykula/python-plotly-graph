bc8f39fd0ad3211fef7eaf36df98037abcd45ebf
amalkhamidov <akoteykula@gmail.com>
2023-05-08 22:50:40 +0200

    lint: added docstrings

--- generate_data.py
+++ generate_data.py
@@ -1,3 +1,7 @@
+"""
+Generating data using Faker Library
+Adding new information to the existing file / create new one
+"""
 import json
 import logging
 import time
@@ -8,10 +12,10 @@ from faker import Faker
 # Initialize the Faker library
 fake = Faker()
 
+
 # Generate fake data for users
-
-
 def generate_person() -> dict:
+    """Generate a dictionary containing fake user data."""
     created_at = fake.date_between(start_date='-5y', end_date='today')
     updated_at = fake.date_between(start_date=created_at, end_date='today')
 
@@ -24,17 +28,17 @@ def generate_person() -> dict:
     }
     return user
 
-
 @dataclass
 class DataGenerator:
+    """A class to generate and save fake user data."""
     person_num: int = 10
     user_data = []
 
     def start(self):
-        # Load existing data from the JSON file
+        """Load existing data from the JSON file and generate new user data."""
         try:
-            with open('fake_users.json', 'r') as f:
-                self.user_data = json.load(f)
+            with open('fake_users.json', 'r', encoding='utf-8') as file:
+                self.user_data = json.load(file)
         except FileNotFoundError:
             pass
 
@@ -42,16 +46,15 @@ class DataGenerator:
             self.user_data.append(generate_person())
 
     def dump(self):
-        # Save the data to a JSON file
-        with open('fake_users.json', 'w') as f:
-            json.dump(self.user_data, f)
+        """Save the generated data to a JSON file."""
+        with open('fake_users.json', 'w', encoding='utf-8') as file:
+            json.dump(self.user_data, file)
 
         print("Fake data generated and added to fake_users.json")
-
 
 while True:
     dg = DataGenerator()
     dg.start()
     dg.dump()
-    logging.info("Added new {} persons".format(dg.person_num))
+    logging.info("Added new %d persons", dg.person_num)
     time.sleep(1)
